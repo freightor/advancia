ffc1e08123095a0429d01b3b49c07570eecf886d
ardenn <ardenn@users.noreply.github.com>
2018-03-06 16:24:50 +0000

    Added transaction list and detail views to stores app

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from transactions.models import Transaction
 
 # Create your views here.
+
+
+def transaction_list(request):
+    store = request.user.storeuser.store
+    queryset = Transaction.objects.filter(store=store)
+    return render(request, "stores/transaction_list.html", {"transactions": queryset})
+
+
+def transaction_detail(request, pk):
+    store = request.user.storeuser.store
+    transaction = get_object_or_404(Transaction.objects.filter(store=store), pk=pk)
+    return render(request, "stores/transaction_detail.html", {"transaction": transaction})
