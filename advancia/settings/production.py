7d6f1327067756d24197450bb252efff87838754
ardenn <ardenn@users.noreply.github.com>
2018-03-09 19:08:09 +0000

    Code refactor

--- advancia/settings/production.py
+++ advancia/settings/production.py
@@ -2,8 +2,7 @@ from .base import *
 import os
 import dj_database_url
 
-ALLOWED_HOSTS = ["*"]
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DATABASES = {
