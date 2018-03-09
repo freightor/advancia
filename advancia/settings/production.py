0a834536c4a6f6db790296e60b041d6fb4662f8c
ardenn <ardenn@users.noreply.github.com>
2018-03-09 19:11:55 +0000

    Code refactor

--- advancia/settings/production.py
+++ advancia/settings/production.py
@@ -3,6 +3,7 @@ import os
 import dj_database_url
 
 DEBUG = True
+ALLOWED_HOSTS = ["advancia.herokuapp.com"]
 SECRET_KEY = os.environ.get('SECRET_KEY')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DATABASES = {
